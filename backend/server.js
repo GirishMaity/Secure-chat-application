a8390d2a597ada2b92b549873e7ef2f11f6548fc
Girish Maity <girishmaity164@gmail.com>
2022-06-17 23:20:24 +0530

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express(); //Instance of express variable
 
@@ -25,6 +26,22 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+//---------------------------DEPLOYMENT----------------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running Successfully");
+  });
+}
+
+//---------------------------DEPLOYMENT----------------------------
 app.use(notFound);
 app.use(errorHandler);
 
