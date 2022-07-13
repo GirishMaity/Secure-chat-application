b575a7ce550b06e9e664989668fd6e6c6988779d
Girish Maity <girishmaity164@gmail.com>
2022-07-13 22:46:51 +0530

    AWS config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://chatdatabase:Sepm69mkl@cluster0.ya5kk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
