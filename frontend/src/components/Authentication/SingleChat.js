d08da67906c3731d6178031db878a11ad77141c5
Girish Maity <girishmaity164@gmail.com>
2023-03-31 13:42:03 +0530

    Added endpoint for deployment

--- frontend/src/components/Authentication/SingleChat.js
+++ frontend/src/components/Authentication/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../../Context/ChatProvider";
-const ENDPOINT = "https://plankton-app-xbip2.ondigitalocean.app/";
+const ENDPOINT = "https://chat-up-wvhi.onrender.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
